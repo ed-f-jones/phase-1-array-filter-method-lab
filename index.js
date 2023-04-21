9be40faf16859747d8febd0bd33fec70002f5709
Edward <ed.f.jones@gmail.com>
2023-04-20 21:32:13 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+// Code your solution her
+function findMatching  (drivers, name)  {
+   const newArray = drivers.filter(function(driver){
+    return driver.toLowerCase() === name.toLowerCase()
+   });
+   return newArray;
+};
+
+
+function fuzzyMatch(drivers, letter) {
+    const myArray = drivers.filter(function (names) {
+        return names[0] === letter[0];
+    });
+    return myArray;
+};
+
+function matchName(names, match) {
+    const newArray = names.filter(function(person){
+        return person.name === match
+    })
+    return newArray;
+}
